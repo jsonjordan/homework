799e492a1a21db12b49f5939bab4fee7bfbd2f39
Jason Jordan <jasonjordan@Jasons-MacBook.local>
2016-05-18 23:43:38 -0400

    implimented method to determine a word score

--- scrabble_refac.rb
+++ scrabble_refac.rb
@@ -3,6 +3,18 @@ require "pry"
 word = nil
 current_player = 1
 
+def scrabble_points
+  {
+      "a" => 1, "e" => 1, "i" => 1, "o" => 1, "u" => 1, "l" => 1, "n" => 1, "s" => 1, "t" => 1, "r" => 1,
+      "d" => 2, "g" => 2,
+      "b" => 3, "c" => 3, "m" => 3, "p" => 3,
+      "f" => 4, "h" => 4, "v" => 4, "w" => 4, "y" => 4,
+      "k" => 5,
+      "j" => 8, "x" => 8,
+      "q" => 10, "z" => 10
+    }
+end
+
 def setup_players
   print "How many players? "
   players = gets.chomp.to_i
@@ -36,8 +48,11 @@ def word_validation_check word
 end
 
 def calc_score word
-  #do stuff
-  10
+  score = 0
+  word.split("").each do |s|
+    score += scrabble_points[s]
+  end
+  score
 end
 
 def update_totals totals, current_player, score
